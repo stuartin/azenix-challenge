80b6f89dc5964e0a7b74a446e9a7e3e7720f5eb3
stuartin <13344713+stuartin@users.noreply.github.com>
2021-10-08 15:36:23 +1100

    add ability to load external file for parsing via argparse

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # azenix-challenge
 
-## Requirements
+## Getting Started
+
+### Prerequisites
+- Python3
+
+### Execution
+To run the log parser you need to pass in a filepath to the log file via the `--file-path` argument. This should be a valid log file that matches the expected layout of the logs being parsed.
+
+```bash
+python .\log_parse\log_parse.py --log-file 'sample-data.log' 
+```
+
+### Logging
+If you need to debug a why parsing fails for a file, you can add the `--verbose` flag to the exection to output the line that is being parsed to troubleshoot.
+
+```bash
+python .\log_parse\log_parse.py --log-file 'sample-data.log' --verbose
+```
+
+## Challenge Details
+### Requirements
 The task is to parse a log file containing HTTP requests and to report on its contents.
 
 For a given log file we want to know:
@@ -8,11 +28,11 @@ For a given log file we want to know:
 - The top 3 most visited URLs
 - The top 3 most active IP addresses
 
-## Deliverables
+### Deliverables
 - For this task you can choose from Java, Python, JavaScript, Kotlin, Go, or C#.
 - There are no restrictions on language features and you are free to use third-party libraries.
 - Please include tests that demonstrate your solution is working.
 - You can submit your solution as a zip file containing the source code or a public git repository.
 
-## Sample Data
+### Sample Data
 [Sample Log](sample-data.log)
--- log_parse/log_parse.py
+++ log_parse/log_parse.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from typing import List
 import re
 import logging
+import argparse
+import sys
 
 @dataclass
 class LogEntry:
@@ -37,7 +39,7 @@ def parse_log_line(line: str) -> LogEntry:
     if len(line) <= 1:
         raise ValueError('The log entry did not contain any data.')
     
-    logging.info(f"\nParsing:\n{line}")
+    logging.info(f"\n{line}")
     
     # Get all fields between quotation marks
     quoted_content_regex = '"([^"]*)"'
@@ -70,27 +72,37 @@ def parse_log_line(line: str) -> LogEntry:
 
     return LogEntry(ip, user, date, response, bytes, method, url, protocol, user_agent)
 
+def get_args(args: List[str]):
+    """
+    Get the arguments passed into our script
+    """
+
+    parser = argparse.ArgumentParser(description="Parse HTTP log requests and return a summary of the data")
+    parser.add_argument("--log-file", required=True, type=argparse.FileType('r', encoding='UTF-8'), help="The file path of the file to parse.")
+    parser.add_argument("--verbose", action='store_true', help="Enable verbose logging to console.")
+
+
+    return parser.parse_args(args)
+
 def main():
     """
     Execute the log parser
     """
 
-    lines = [
-        '177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] "GET /intranet-analytics/ HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7"',
-        '168.41.191.40 - - [09/Jul/2018:10:11:30 +0200] "GET http://example.net/faq/ HTTP/1.1" 200 3574 "-" "Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"',
-        '168.41.191.41 - - [11/Jul/2018:17:41:30 +0200] "GET /this/page/does/not/exist/ HTTP/1.1" 404 3574 "-" "Mozilla/5.0 (Linux; U; Android 2.3.5; en-us; HTC Vision Build/GRI40) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"'
-    ]
+    args = get_args(sys.argv[1:])
+    if args.verbose:
+        logging.basicConfig( level=logging.INFO )
 
     try:
-
-        log_entries = LogEntries([parse_log_line(line) for line in lines])
-        print(f"{[i.ip for i in log_entries.logs]}")
+        log_entries = LogEntries([parse_log_line(line) for line in args.log_file.readlines()])
+        print(f"{[i.date.strftime('%D') for i in log_entries.logs]}")
     
     except ValueError as error:
         print('Failed to parse the log file.\n')
         print(error)
 
-    pass
+    finally:
+        args.log_file.close()
 
 if __name__ == "__main__":
     main()
